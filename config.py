157cf775e595a02c05f646cf4927113b83e31f79
itszshivam <139079474+itszshivam@users.noreply.github.com>
2023-09-17 00:16:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5174492401").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itszshivam/SK_musicbot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itszshivam/Perfectmusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
